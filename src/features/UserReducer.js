9c6c94b5c886203c47ff87b538a322c737a6a225
Vignesh100799 <baluvignesh422@gmail.com>
2023-12-22 09:23:55 +0530

    Changes

--- src/features/UserReducer.js
+++ src/features/UserReducer.js
@@ -1,41 +1,21 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { createSlice, } from '@reduxjs/toolkit';
 import axios from 'axios';
-import { toast } from 'react-toastify';
-import { config } from '../config/config';
-
-export const registerUser = createAsyncThunk('users/registerUser', async (userData, { rejectWithValue }) => {
-  try {
-    const response = await axios.post(`${config.userApi}/register`, userData);
-    return response.data;
-  } catch (error) {
-    return rejectWithValue(error.response.data);
-  }
-});
 
 const UserSlice = createSlice({
-  name: 'users',
-  initialState: {
+    name: 'users',
+    initialState: {
+        loading:false,
+        showPassword:false
+    },
+    reducers: {
+        setLoading:(state,action)=>{
+            state.loading = action.payload
+        },
+        setShowPassword:(state,action)=>{
+            state.showPassword = action.payload;
+          }
+    },
     
-},
-  reducers: {
-  
-  },
-  extraReducers: (builder) => {
-    builder
-      .addCase(registerUser.fulfilled, (state, action) => {
-        
-        toast.success('Registration Successfully done 😃!', {
-          position: 'top-center',
-        });
-      })
-      .addCase(registerUser.rejected, (state, action) => {
-       
-        console.error('Error during registration:', action.payload);
-        toast.error('Error during registration. Please try again.', {
-          position: 'top-center',
-        });
-      });
-  },
 });
-
+export const {setLoading,setShowPassword} =  UserSlice.actions
 export default UserSlice.reducer;
